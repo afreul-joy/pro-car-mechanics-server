00d3d07ad26705eba6a86ca65cf87337efb5733a
Afreul kaium Joy <afreuljoy@gmail.com>
2021-10-31 12:49:52 +0600

    single api added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
-require('dotenv').config()
 const { MongoClient } = require('mongodb');
+const objectId = require('mongodb').ObjectId;
+
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 const port = 5000
+
+app.use(cors());
+app.use(express.json());
+  
 
 const uri =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vljpp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -12,7 +19,38 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     await client.connect();
-   console.log("Connected to dataBase Yahoo");
+  //  console.log("Connected to dataBase Yahoo")
+    const database = client.db("carMechanics");
+    const servicesCollection = database.collection("services");
+
+    //get api 
+    app.get('/services', async (req, res) => {
+      const cursor = servicesCollection.find({})
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+    // Get Single Service 
+    app.get('/services/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log('get id hitting',id);
+      const query = { _id: objectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service)
+
+      })
+
+    // post api 
+    app.post('/services', async (req, res) => {
+      const service = req.body;
+      // console.log('hit the post api',service);
+      // res.send('post hitted')
+
+      const result = await servicesCollection.insertOne(service)
+      console.log(result);
+      res.json(result)
+    })
+
+
   } finally {
    // await client.close();
   }
